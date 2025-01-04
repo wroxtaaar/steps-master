2c9aab521e4f5d27396e8dc673f486b339926fa9
wasiq <wroxtaar@gmail.com>
2025-01-04 12:17:03 +0530

    seperated the button component

--- src/App.js
+++ src/App.js
@@ -14,41 +14,55 @@ export default function App() {
   // let step = 1;
 
   function goNext() {
-    if(step < 3)
-    setStep(step + 1);
+    if (step < 3) setStep(step + 1);
   }
 
   function goback() {
-    if(step > 1)
-    setStep(step - 1);
+    if (step > 1) setStep(step - 1);
   }
 
   return (
-    < >    
-<button className="close" onClick={() => {setIsOpen(!isOpen)}}>&times;</button>
-   { isOpen && 
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step === 3 ? "active" : ""}>3</div>
-      </div>
+    <>
+      <button
+        className="close"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step === 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button style={style} onClick={() => {goback()}}>
+          <div className="buttons">
+            {/* <button style={style} onClick={() => {goback()}}>
           Previous
         </button>
         <button style={style} onClick={() => {goNext()}}>
           Next
-        </button>
-      </div>
-    </div>
-    }
-
+        </button> */}
+            <Button style={style} direction={goback} text="Previous" />
+            <Button style={style} direction={goNext} text="Next" />
+          </div>
+        </div>
+      )}
     </>
+  );
+}
+
+function Button({ style, direction, text }) {
+  return (
+    <button style={style} onClick={direction}>
+      {text}
+    </button>
   );
 }
