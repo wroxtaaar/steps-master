2962ba0ec92baa7722661b4e43710a32ca3aff02
wasiq <wroxtaar@gmail.com>
2025-01-04 11:55:36 +0530

    start again

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑"
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  const style = { backgroundColor: "#7950f2", color: "#fff" };
+
+  // let step = 1;
+
+  function goNext() {
+    if(step < 3)
+    setStep(step + 1);
+  }
+
+  function goback() {
+    if(step > 1)
+    setStep(step - 1);
+  }
+
+  return (
+    < >    
+<button className="close" onClick={() => {setIsOpen(!isOpen)}}>&times;</button>
+   { isOpen && 
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step === 3 ? "active" : ""}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={style} onClick={() => {goback()}}>
+          Previous
+        </button>
+        <button style={style} onClick={() => {goNext()}}>
+          Next
+        </button>
+      </div>
+    </div>
+    }
+
+    </>
+  );
+}
